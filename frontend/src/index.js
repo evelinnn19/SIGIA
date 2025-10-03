d786c1b9c940b46401bc9421d3e0c1de8a191da9
Razzolini Khiara <razzolinikhiara@gmail.com>
2025-10-03 15:22:21 -0300

    Modificación de Tailwind y avance en pantalla Notas cargadas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hola Node.js!");
+ document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const usuario = document.querySelector('input[name="usuario"]').value;
+        const password = document.querySelector('input[name="contraseña"]').value;
+        
+        if (usuario && password) {
+          window.location.href = 'notas-cargadas.html';
+        } else {
+          alert('Por favor completa todos los campos');
+        }
+      });
